43d60989769ebf6623fa15621bbbe923638dcf21
Clarence Escoto <116655310+Clarkyy999@users.noreply.github.com>
2024-11-05 22:39:13 +0800

    accInfo added
    sa input box yung current value lalabas tapos pag nag type sila ibig sabihin eedit nila, pag pinindot yung update account dapat may confirmation message

---
+++ client/accInfo.php
@@ -0,0 +1,40 @@
+<?php
+include 'header.php';
+?>          
+
+<link rel="stylesheet" href="../CSS/accInfo.css">
+
+
+<div class="body">
+
+    <div class="account-info-container">
+        <h1>Account Info</h1>
+        <div class="account-info-box">
+            <div class="info-section">
+
+                <label>Username:</label>
+                <input type="text" placeholder="Current value">
+                
+                <label>Full Name:</label>
+                <input type="text" placeholder="Current value">
+                
+                <label>Contact #:</label>
+                <input type="text" placeholder="Current value">
+                
+                <label>Password:</label>
+                <input type="text" placeholder="Current value">
+
+                <div class="buttons">
+                    <button class="edit-btn">Update Account</button>
+                </div>
+            </div>
+
+            <div class="stats-section">
+                <p><strong>Total Orders:</strong> 23</p>
+                <p><strong>Available Points:</strong> 3</p>
+            </div>
+
+        </div>
+    </div>
+
+</div>
