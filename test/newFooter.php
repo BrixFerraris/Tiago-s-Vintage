2389ec87f5ff3d342bbd903fc06bed4c90311c0b
Ferrarispo <118862242+Ferrarispo@users.noreply.github.com>
2024-11-10 00:45:35 +0800

    minor changes
    Discount/freebie sa checkout (Wala pang front-end)
    
    Acc points sa admin side
    
    Complete/cancelled
    
    Reviews customer/admin side
    
    Reports
    
    Analytics
    
    Notif

--- test/newFooter.php
+++ test/newFooter.php
@@ -11,25 +11,63 @@
 <body>
     <footer>
         <div class="footer-info">
+        <span>Tiago's Vintage Boutique:</span>
+            <div class="social-icons">
+                <a href="https://www.facebook.com/profile.php?id=100063803240125"><i class="fa-brands fa-facebook"></i></a>
+                <a href="https://www.instagram.com/tiagos_vintage_/"><i class="fa-brands fa-instagram"></i></a>
+            </div>
         </div>
 
         <div class="footer-container">
             <div class="rows">
-                <div class="row1">
-                    <h3>TIAGO'S VINTAGE</h3>
-                    <p>offers a seamless online shopping experience for vintage enthusiasts.</p>
+                <div class="row1"><img src="../assets/tiagos-removebg-preview 1.png" alt="">
+                    <p>Tiago's Vintage provides a seamless online shopping experience for vintage enthusiasts, offering a carefully curated selection of timeless pieces. With an intuitive interface and smooth navigation, it ensures that finding and purchasing unique items is both enjoyable and effortless.</p>
                 </div>  
                 <div class="row3">
-                    <h3>ADDRESS:</h3>
+                <h3>ADDRESS:</h3>
                     <p><i class="fas fa-location"></i> Bayan Luma, Imus City, Cavite</p>
+
+                    <div class="mapouter">
+                        <div class="gmap_canvas">
+                            <iframe class="gmap_iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=600&amp;height=400&amp;hl=en&amp;q=546 General Yengco St Imus, Calabarzon&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed">
+                            </iframe>
+                        </div>
+                        </div>
                 </div>
                 <div class="row4">
-                    <h3>CONTACT US:</h3>
+                <h3>CONTACT US:</h3>
                     <p><i class="fa-brands fa-facebook"></i> Tiago's Vintage</p>
                     <p><i class="fa-brands fa-instagram"></i> tiagos_vintage_</p>
                     <p><i class="fas fa-phone"></i> 0995 795 6315</p>
+                        </div>
+                        <style>
+                             .mapouter {
+        position: relative;
+        width: 600px; /* Adjust the width to your liking */
+        height: 350px; /* Adjust the height to your liking */
+    }
+
+    .gmap_canvas {
+        overflow: hidden;
+        background: none!important;
+        width: 80%; /* This will make the canvas match the parent size */
+        height: 80%; /* Same as above */
+    }
+
+    .gmap_iframe {
+        width: 80%!important; /* Make iframe take full width of the parent */
+        height: 80%!important; /* Make iframe take full height of the parent */
+    }
+                                </style>
+                                
+                    
                 </div>
             </div>
+        </div>
+        <div class="footer-bottom">
+            <p>© 2024 Copyright:
+                <a href="http://tiago-vintage.helioho.st/client/landing.php">Tiagos-vintage.helioho.st</a>
+            </p>
         </div>
     </footer>
 </body>
